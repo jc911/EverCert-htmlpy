eefc7da1a4171f1c900b765ded519bfda3316401
jc911 <jichi663@126.com>
2017-06-13 20:32:04 +0800

    1
    1

--- back_end.py
+++ back_end.py
@@ -1,6 +1,7 @@
 import htmlPy
 import requests
 import json
+import re
 
 class BackEnd(htmlPy.Object):
 	def __init__(self, app):
@@ -43,7 +44,54 @@ class BackEnd(htmlPy.Object):
 		except BaseException, e:
 			login_state = e
 		finally:
-			self.app.template = ("./index.html", {"login_state": login_state,"userName":userName,"passWord":passWord})
+			#self.app.template.render(login_state="test")
+			self.app.template = ("index.html", {"login_state": login_state,"userName":userName,"passWord":passWord})
 
+	@htmlPy.Slot()
+	def getCookie(self):
+		#print(self.app.html)
+		Cookie = "null"
+		print(self.app.template)
+		login_state = self.app.template[1]['login_state']
+		if re.search(r'Cookie: (.*)',login_state) != None:
+			Cookie = re.search(r'Cookie: (.*)',login_state).group(1)
+		print(Cookie)
+		return Cookie
 
+	@htmlPy.Slot()
+	def firstExamineList(self):
+		if getCookie != "null":
+			try:
+				r = requests.post("http://202.108.212.74:8000/cnvd_admin/login/loginCheck", allow_redirects=False,
+								  data=payload, proxies=proxies, timeout=3)
+				print(r.text)
+				print(r.status_code)
+				print(r.headers['Location'])
+				if r.status_code == 302 and r.headers[
+					'Location'] == 'http://202.108.212.74:8000/cnvd_admin/login/loginFail':
+					print('login failed')
+					login_state = 'login failed'
+				if r.status_code == 302 and r.headers[
+					'Location'] == 'http://202.108.212.74:8000/cnvd_admin/login/loginSuccess':
+					print('login success')
+					print(r.cookies['JSESSIONID'])
+					login_state = 'Cookie: ' + r.cookies['JSESSIONID']
+					return r.cookies
+			except BaseException, e:
+				login_state = e
+			finally:
+				# self.app.template.render(login_state="test")
+				self.app.template = (
+				"index.html", {"login_state": login_state, "userName": userName, "passWord": passWord})
+
+		cnvd_bianhaos={
+			123,321,111,222,333
+		}
+		template_dic = self.app.template[1]
+		print(template_dic)
+		template_dic['cnvd_bianhaos'] = cnvd_bianhaos
+		print(template_dic)
+		self.app.template = ("index.html", template_dic)
+
+		#http://202.108.212.74:8000/cnvd_admin/flaw/firstExamineList
 
